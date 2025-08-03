e376327bf2626990ab3f001c83a5b6c0ae0b746d
MukeshPyatla <mukeshyadav9989@gmail.com>
2025-08-03 13:22:11 -0500

    Add Streamlit Cloud configuration for Python 3.13 deployment

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+# Import and run the main app
+from ui.app import * 
